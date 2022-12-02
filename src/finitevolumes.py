50ff37f19d7885bd9b9adac81b60e4e502522290
ygorschka <ygorschka@gmail.com>
2022-12-02 15:00:14 +0100

    Ajout noms changes

---
+++ src/finitevolumes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+class FiniteVolumes:
+    """
+        Finite volumes schemes
+    """
+
+    def __init__(self):
+        None
---
+++ src/timeschemes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+class TimeSchemes:
+    """
+        Time schemes
+    """
+
+    def __init__(self):
+        None
