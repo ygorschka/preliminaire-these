4d652b420f94c2c09ae41add0122db9481ef9d58
ygorschka <ygorschka@gmail.com>
2022-12-02 14:59:09 +0100

    Changement de nom

---
+++ src/main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+from 
+
+def main():
+    print("cc")
+
+if __name__ == "__main__":
+    main()
---
+++ src/parameters.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+class Parameters:
+    """
+        Parameters of the problem
+    """
+
+    def __init__(self):
+        None
